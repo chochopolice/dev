520436b5b2d6148da71907d68bc6d8a1e8c34186
chochopolice <o06d012@yahoo.co.jp>
2026-05-22 21:26:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -163,6 +163,20 @@ window.removeTask = async function(id) {
   await loadTasks();
 };
 
+function getStatusClass(taskStatus) {
+  switch (taskStatus) {
+    case "未対応":
+    case "未完了":
+      return "status-todo";
+    case "対応中":
+      return "status-doing";
+    case "完了":
+      return "status-done";
+    default:
+      return "status-done";
+  }
+}
+
 function clearForm() {
   taskId.value = "";
   title.value = "";
@@ -172,6 +186,7 @@ function clearForm() {
   status.value = "未対応";
   memo.value = "";
 }
+
 
 
 function getStatusClass(taskStatus) {
